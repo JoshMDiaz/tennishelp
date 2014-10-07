da5bf25f89a250aa9e6061584396d7685c82153c
Josh M. Diaz <joshua.michael.diaz@gmail.com>
2014-10-06 18:06:54 -0600

    adding tab-video

--- tennishelpApp/www/js/controllers.js
+++ tennishelpApp/www/js/controllers.js
@@ -3,16 +3,19 @@ angular.module('starter.controllers', [])
 .controller('HomeCtrl', function($scope) {
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+// .controller('FriendsCtrl', function($scope, Friends) {
+//   $scope.friends = Friends.all();
+// })
+//
+// .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+//   $scope.friend = Friends.get($stateParams.friendId);
+// })
 
 .controller('StatsCtrl', function($scope) {
 })
 
 .controller('ResultsCtrl', function($scope) {
+})
+
+.controller('VideoCtrl', function($scope) {
 });
