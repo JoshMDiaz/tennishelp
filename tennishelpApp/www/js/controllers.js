934a97bc7fdbfec273aac4187c0f071a02a04af0
Josh M. Diaz <joshua.michael.diaz@gmail.com>
2014-11-03 18:59:08 -0700

    Trying to add angular. Working on setting up controllers and getting ng-repeat to work. Not working yet.

--- tennishelpApp/www/js/controllers.js
+++ tennishelpApp/www/js/controllers.js
@@ -3,22 +3,15 @@ angular.module('tennishelp.controllers', [])
 .controller('HomeCtrl', function($scope) {
 })
 
-// .controller('FriendsCtrl', function($scope, Friends) {
-//   $scope.friends = Friends.all();
-// })
-//
-// .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-//   $scope.friend = Friends.get($stateParams.friendId);
-// })
-
 .controller('StatsCtrl', function($scope) {
-  
+
 })
 
 .controller('ResultsCtrl', function($scope) {
 })
 
 .controller('InProgressCtrl', function($scope) {
+
 })
 
 .controller('MatchStatsCtrl', function($scope) {
