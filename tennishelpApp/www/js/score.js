e6d0201478c33689d1a28428d60b33b3321ef10f
Josh M. Diaz <joshua.michael.diaz@gmail.com>
2014-11-22 22:11:09 -0700

    I can't figure out how to add a simple alert. FAIL.

--- tennishelpApp/www/js/score.js
+++ tennishelpApp/www/js/score.js
@@ -2,15 +2,15 @@
 
   // Adding the name of the Players in the scoreboard
 
-  if ({{player1.name}} = true) {
-   result = {{player1.name}};
-  } else {
-     result = "player 1";
-  }
-
-
-  if ({{player2.name}} = true) {
-   result = {{player2.name}};
-  } else {
-     result = "player 2";
-  }
+  // if ({{player1.name}} = true) {
+  //  result = {{player1.name}};
+  // } else {
+  //    result = "player 1";
+  // }
+  //
+  //
+  // if ({{player2.name}} = true) {
+  //  result = {{player2.name}};
+  // } else {
+  //    result = "player 2";
+  // }
